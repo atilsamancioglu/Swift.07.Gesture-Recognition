ec7bc404804b33403d9f8aee1451873a2efa939e
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:12:36 +0300

    final version

--- Gesture Recognition/ViewController.swift
+++ Gesture Recognition/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var myInteger = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.changePicture))
+
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func changePicture() {
+        
+        if myInteger == 0 {
+            imageView.image = UIImage(named: "kirk.jpg")
+            nameLabel.text = "Kirk Hammett"
+            myInteger += 1
+        } else if myInteger == 1 {
+            imageView.image = UIImage(named: "james.jpg")
+            nameLabel.text = "James Hetfield"
+            myInteger -= 1
+        }
+        
     }
 
 
